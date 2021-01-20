cdc811c5cd8f32ec95a78d92e4036fa88b859539
Nico Paulino <nicopaulino8@gmail.com>
2021-01-20 16:51:23 -0600

    Add Initial Route

--- angular-crash-todolist/src/app/app-routing.module.ts
+++ angular-crash-todolist/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
 
-const routes: Routes = [];
+  { path: '', component: TodosComponent },
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
